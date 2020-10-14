7c1e9e93aece0a1f1d49f7a5e7aefc21888fbd85
Tierry Ray <tierry.ray@ifood.com.br>
2020-10-14 11:03:37 -0300

    feat: logo component done with tests

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,9 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Logo from '.'
+import Logo, { LogoProps } from '.'
+import { Meta, Story } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Logo',
   component: Logo
 } as Meta
 
-export const Default: Story = () => <Logo />
+export const Default: Story<LogoProps> = (args) => <Logo {...args} />
