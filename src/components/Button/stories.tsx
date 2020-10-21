864462c8a5175a6bfb96c798d335c4ece80d11c7
Tierry Ray <tierry.ray@ifood.com.br>
2020-10-21 11:01:54 -0300

    feat: button completed with tests

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+      type: ''
+    }
+  }
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy Now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
+}
