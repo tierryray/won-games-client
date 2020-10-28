e0470b4f3ca784893b9aa4fc245fbcccf1e0534a
Tierry Ray <tierry.ray@ifood.com.br>
2020-10-28 10:36:56 -0300

    feat(footer): footer completed

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,22 @@
+import Footer from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { screen } from '@testing-library/react'
+
+describe('<Footer />', () => {
+  it('should render 4 columns topics', () => {
+    const { container } = renderWithTheme(<Footer />)
+
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /location/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
