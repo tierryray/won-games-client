5248a1d3f5820ce4f4fefd56af06f29dd1ad07bc
Tierry Ray <tierry.ray@ifood.com.br>
2020-11-04 11:13:12 -0300

    feat(highlight): component and tests done

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,8 +1,36 @@
 import * as S from './styles'
 
-const Highlight = () => (
-  <S.Wrapper>
-    <h1>Highlight</h1>
+import Button from 'components/Button'
+
+export type HighlightProps = {
+  title: string
+  subtitle: string
+  backgroundImage: string
+  floatImage?: string
+  buttonLabel: string
+  buttonLink: string
+  alignment?: 'right' | 'left'
+}
+
+const Highlight = ({
+  title,
+  subtitle,
+  backgroundImage,
+  floatImage,
+  buttonLabel,
+  buttonLink,
+  alignment = 'right'
+}: HighlightProps) => (
+  <S.Wrapper backgroundImage={backgroundImage} alignment={alignment}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+    <S.Content>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Content>
   </S.Wrapper>
 )
 
