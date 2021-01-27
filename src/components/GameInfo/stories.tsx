1477be2705b94372327eac5d099c4107f7779149
Tierry Ray <tierry.ray@ifood.com.br>
2021-01-27 10:02:48 -0300

    feat(game-info): game info component with tests and styles

---
+++ src/components/GameInfo/stories.tsx
@@ -0,0 +1,21 @@
+import GameInfo, { GameInfoProps } from '.'
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import mockGame from './mock'
+
+export default {
+  title: 'GameInfo',
+  component: GameInfo,
+  args: mockGame,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '144rem', padding: '1.5rem', margin: 'auto' }}>
+    <GameInfo {...args} />
+  </div>
+)
